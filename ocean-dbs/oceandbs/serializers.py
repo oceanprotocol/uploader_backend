f915cf0ca52d7daf3120f69f642c3b690f3df1b9
Benoit Alessandroni <benoit@happy-dev.fr>
2022-11-20 19:06:55 +0100

    draft: of the quote creation method

--- ocean-dbs/oceandbs/serializers.py
+++ ocean-dbs/oceandbs/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import AcceptedToken, Quote, Storage, PaymentMethod, PAYMENT_STATUS, UPLOAD_CODE
+from .models import AcceptedToken, Quote, Storage, PaymentMethod, Payment, PAYMENT_STATUS, UPLOAD_CODE
 
 class TokensSerializer(serializers.ModelSerializer):
   def to_representation(self, instance):
@@ -23,8 +23,50 @@ class StorageSerializer(serializers.ModelSerializer):
     model = Storage
     fields = ['type', 'description', 'payment_methods']
 
+class PaymentSerializer(serializers.ModelSerializer):
+  payment_method = PaymentMethodSerializer()
+
+  class Meta:
+    model = Payment
+    fields = ['payment_method', 'wallet_address']
+
+  def create(self, validated_data):
+    payment_method_data = validated_data.pop('payment')
+    payment = Payment.objects.create(**validated_data)
+    for method in payment_method_data:
+        PaymentMethod.objects.create(payment=payment, **method)
+    return payment
 
 class QuoteSerializer(serializers.ModelSerializer):
-    class Meta:
-      model = Quote
-      fields = ['id', 'storage', 'duration', 'payment', 'wallet_address', 'upload_status']
+  payment = PaymentSerializer()
+  class Meta:
+    model = Quote
+    fields = ['id', 'storage', 'duration', 'payment', 'wallet_address', 'upload_status']
+
+  def create(self, validated_data):
+    #TODO: make sure validated data or data contains type and files
+    #TODO: from type, retrieve associated storage object
+    # If not exists, raise error
+    
+    # From files, retrieve individual file size
+    
+    # For the given type of storage, make a call to the associated service API (mock first) to retrieve a cost associated with that
+    # Save the cost/payment request
+    
+    # For payment method, check if it exits already. If so, associate it with the payment object instead of
+    # creating the new payment with status still to execute
+    print(validated_data)
+    storage = Storage.objects.get(type=validated_data.pop('type'))
+    print(storage)
+    payment_data = validated_data.pop('payment')
+    quote = Quote.objects.create(storage=storage, **validated_data)
+    payment_method = payment_data['payment_method']
+    method = PaymentMethod.objects.create(**payment_method)
+    payment_data['payment_method'] = method
+    Payment.objects.create(quote=quote, **payment_data)
+    
+    print(PaymentMethod.objects.all())
+    print(Payment.objects.all())
+    print(Quote.objects.all())
+    return quote
+
--- ocean-dbs/oceandbs/tests/tests_quote.py
+++ ocean-dbs/oceandbs/tests/tests_quote.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIRequestFactory, APIClient, APITestCase
-# Create your tests here.
+from rest_framework.utils import json
 
 # Using the standard RequestFactory API to create a form POST request
 class TestGetQuoteEndpoint(APITestCase):
@@ -11,7 +11,7 @@ class TestGetQuoteEndpoint(APITestCase):
     self.factory = APIRequestFactory()
     self.client = APIClient()
 
-  def test_get_info_endpoint(self):
+  def test_get_quotes_endpoint(self):
     response = self.client.get('/quotes/')
 
     # Assert proper HTTP status code
@@ -23,3 +23,28 @@ class TestGetQuoteEndpoint(APITestCase):
     # Assert content of the response itself, pure JSON
     # self.assertEqual(response.data['content'], '')
 
+  def test_quote_creation(self):
+    body = {
+      "type": "filecoin",
+      "files": [
+        {"length":2343545},
+        {"length":2343545}
+      ],
+      "duration": 4353545453,
+      "payment": {
+          "payment_method": {
+            "chain_id": 1,
+          },
+          "wallet_address": "0xOCEAN_on_MAINNET"
+      },
+      "userAddress": "0x456"
+    }
+
+    response = self.client.post('/quotes/', data=json.dumps(body), content_type='application/json')
+    print(response.data)
+    # self.assertEqual(response.status_code, 200)
+    # self.assertNotEqual(response.status_code, 400)
+    self.assertEqual(len(response.data), 1)
+    print(response.data)
+    self.assertExists(response.data[0]['payment'])
+    self.assertNotExists(response.data[0]['payment'][0])
