8c7045bc695215fe20733ff3059f267788ca71d9
Benoit Alessandroni <benoit@happy-dev.fr>
2023-02-08 18:20:33 +0100

    fix: dependencies

--- server/oceandbs/utils.py
+++ server/oceandbs/utils.py
@@ -2,7 +2,8 @@ from rest_framework.response import Response
 from django.utils import timezone
 from datetime import datetime
 import hashlib
-
+from web3.auto import w3
+from eth_account.messages import encode_defunct
 
 def check_params_validity(params, quote):
   if not all(key in params for key in ('nonce', 'signature')):
@@ -16,16 +17,19 @@ def check_params_validity(params, quote):
   if str(round(quote.nonce.timestamp())) > params['nonce'][0]:
     return Response("Nonce value invalid.", status=400)
 
+  message = str(quote.quoteId) + str(params['nonce'][0])
+  message = encode_defunct(text=message)
   # Use verifyMessage from web3/ethereum API
-  
+  check_signature = w3.eth.account.recover_message(message, signature=params['signature'][0])
+
+  if check_signature:
+    quote.nonce = datetime.fromtimestamp(int(params['nonce'][0]), timezone.utc)
+    quote.save()
+
+    return True
   # Then decode message and
   # Check signature
-  sha256_hash = hashlib.sha256((str(quote.quoteId) + str(params['nonce'][0])).encode('utf-8')).hexdigest()
-  if not sha256_hash == params['signature'][0]:
-    return Response("Invalid signature.", status=400)
-
-
-  quote.nonce = datetime.fromtimestamp(int(params['nonce'][0]), timezone.utc)
-  quote.save()
-
-  return True
+  # sha256_hash = hashlib.sha256(str(quote.quoteId) + str(params['nonce'][0]))
+  # if not sha256_hash == params['signature'][0]:
+  #   return Response("Invalid signature.", status=400)
+  return False
