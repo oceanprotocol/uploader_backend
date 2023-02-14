1ff1eeda0db4dcdc926d6fcb51a21543b08f7a15
Benoit Alessandroni <benoit@happy-dev.fr>
2023-02-14 10:40:43 +0100

    fix: previous

--- server/server/settings.py
+++ server/server/settings.py
@@ -173,11 +173,7 @@ SPECTACULAR_SETTINGS = {
 FILECOIN_SERVICE_URL = 'http://localhost:3000'
 ARWEAVE_SERVICE_URL = 'https://storage.arweave.com'
 
-TEST_PRIVATE_KEY = os.environ.get("TEST_PRIVATE_KEY")
-PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
-TOKEN_ADDRESS = os.environ.get("TOKEN_ADDRESS")
-
-if os.getenv('GITHUB_WORKFLOW'):
-    TEST_PRIVATE_KEY = os.getenv("TEST_PRIVATE_KEY")
-    PRIVATE_KEY = os.getenv("PRIVATE_KEY")
-    TOKEN_ADDRESS = os.getenv("TOKEN_ADDRESS")
+if not os.getenv('GITHUB_WORKFLOW'):
+    TEST_PRIVATE_KEY = os.environ.get("TEST_PRIVATE_KEY")
+    PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
+    TOKEN_ADDRESS = os.environ.get("TOKEN_ADDRESS")
