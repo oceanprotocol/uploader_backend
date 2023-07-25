bdae836368f4900e80ce8da10406733f09499e10
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-07-25 11:41:43 +0300

    Updating paths

--- server/oceandbs/urls.py
+++ server/oceandbs/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.StorageListView.as_view(), name="info"),
-    path('register', views.StorageCreationView.as_view(), name="service-creation"),
-    path('getStatus', views.QuoteStatusView.as_view(), name="status"),
-    path('getLink', views.QuoteLink.as_view(), name="link"),
-    path('getQuote', views.QuoteCreationView.as_view()),
-    path('upload', views.UploadFile.as_view())
+    path('/', views.StorageListView.as_view(), name="info"),
+    path('/register', views.StorageCreationView.as_view(), name="service-creation"),
+    path('/getStatus', views.QuoteStatusView.as_view(), name="status"),
+    path('/getLink', views.QuoteLink.as_view(), name="link"),
+    path('/getQuote', views.QuoteCreationView.as_view()),
+    path('/upload', views.UploadFile.as_view())
 ]
