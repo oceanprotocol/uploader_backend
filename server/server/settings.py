0cd6b8f14493d7bf7117c19ed9079e0fcddea85c
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-08-31 12:49:04 +0300

    Add middleware for uploads.

--- server/server/settings.py
+++ server/server/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.multipart.MultiPartParser',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
