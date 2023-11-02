54ce015312aa0e9fbfdcaf18f0ae13ddef84eea7
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-11-02 16:24:01 +0300

    Adding logs

--- server/oceandbs/serializers.py
+++ server/oceandbs/serializers.py
@@ -16,9 +16,14 @@ class PaymentMethodSerializer(serializers.ModelSerializer):
         fields = ['chainId', 'acceptedTokens']
 
     def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['acceptedTokens'] = [{"title": token.title, "value": token.value} for token in instance.acceptedTokens.all()]
-        return representation
+        print("to_representation called in PaymentMethodSerializer")
+        try:
+            representation = super().to_representation(instance)
+            representation['acceptedTokens'] = [{"title": token.title, "value": token.value} for token in instance.acceptedTokens.all()]
+            return representation
+        except Exception as e:
+            print(f"Error in PaymentMethodSerializer to_representation: {e}")
+            raise e
 
 class PaymentSerializer(serializers.ModelSerializer):
     paymentMethod = PaymentMethodSerializer()
@@ -49,16 +54,24 @@ class CreateStorageSerializer(serializers.ModelSerializer):
         fields = ['type', 'description', 'url', 'payment']
 
     def create(self, validated_data):
-        payment_method_data = validated_data.pop('payment')
-        storage = Storage.objects.create(**validated_data)
+        print("create called in CreateStorageSerializer")
+        try:
+            payment_method_data = validated_data.pop('payment')
+            storage = Storage.objects.create(**validated_data)
+            print(f"Storage created: {storage}")
 
-        for method_data in payment_method_data:
-            accepted_tokens_data = method_data.pop('acceptedTokens')
-            method = PaymentMethod.objects.create(storage=storage, **method_data)
-            for accepted_token_data in accepted_tokens_data:
-                AcceptedToken.objects.create(paymentMethod=method, **accepted_token_data)
+            for method_data in payment_method_data:
+                accepted_tokens_data = method_data.pop('acceptedTokens')
+                method = PaymentMethod.objects.create(storage=storage, **method_data)
+                print(f"PaymentMethod created: {method}")
+                for accepted_token_data in accepted_tokens_data:
+                    AcceptedToken.objects.create(paymentMethod=method, **accepted_token_data)
+                    print(f"AcceptedToken created with data: {accepted_token_data}")
 
-        return storage
+            return storage
+        except Exception as e:
+            print(f"Error in CreateStorageSerializer create: {e}")
+            raise e
 
 class QuoteSerializer(serializers.ModelSerializer):
     payment = PaymentSerializer()
@@ -69,19 +82,32 @@ class QuoteSerializer(serializers.ModelSerializer):
         fields = ['storage', 'tokenAmount', 'quoteId', 'duration', 'tokenAddress', 'approveAddress', 'status', 'files', 'payment']
 
     def create(self, validated_data):
-        payment_data = validated_data.pop('payment')
-        files_data = validated_data.pop('files')
-        quote = Quote.objects.create(**validated_data)
+        print("create called in QuoteSerializer")
+        try:
+            payment_data = validated_data.pop('payment')
+            files_data = validated_data.pop('files')
+            quote = Quote.objects.create(**validated_data)
+            print(f"Quote created: {quote}")
 
-        payment_method_data = payment_data.pop('paymentMethod')
-        method = PaymentMethod.objects.filter(storage=validated_data['storage'], chainId=payment_method_data['chainId']).first()
-        payment_data['paymentMethod'] = method
-        payment = Payment.objects.create(quote=quote, **payment_data)
+            payment_method_data = payment_data.pop('paymentMethod')
+            method = PaymentMethod.objects.filter(storage=validated_data['storage'], chainId=payment_method_data['chainId']).first()
+            if method:
+                print(f"PaymentMethod found: {method}")
+            else:
+                print(f"No PaymentMethod found for chainId: {payment_method_data['chainId']}")
 
-        quote.payment = payment
-        quote.save()
+            payment_data['paymentMethod'] = method
+            payment = Payment.objects.create(quote=quote, **payment_data)
+            print(f"Payment created: {payment}")
 
-        for file_data in files_data:
-            File.objects.create(quote=quote, **file_data)
+            quote.payment = payment
+            quote.save()
 
-        return quote
+            for file_data in files_data:
+                File.objects.create(quote=quote, **file_data)
+                print(f"File created with data: {file_data}")
+
+            return quote
+        except Exception as e:
+            print(f"Error in QuoteSerializer create: {e}")
+            raise e
